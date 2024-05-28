d42344724b2ca80aeb97f14314ecb6ac43723b09
Elliot Taylor <elliot@raison.co>
2024-05-28 11:33:32 +0100

    Update activity-log-card.blade.php

--- resources/views/livewire/activity-log-card.blade.php
+++ resources/views/livewire/activity-log-card.blade.php
@@ -2,9 +2,53 @@
     <x-pulse::card-header name="{{$header}}">
     </x-pulse::card-header>
 
-    <x-pulse::scroll :expand="$expand">
-        <canvas id="{{ $chartId }}"></canvas>
-    </x-pulse::scroll>
+    @php
+        $type = $type ?? 'list';
+    @endphp
+
+    @if($type === 'chart')
+        <x-pulse::scroll :expand="$expand">
+            <canvas id="{{ $chartId }}"></canvas>
+        </x-pulse::scroll>
+    @else
+        <x-pulse::scroll :expand="$expand" wire:poll.5s="">
+            <div class="flex flex-col gap-6">
+                <div>
+                    <x-pulse::table>
+                        <colgroup>
+                            <col width="100%"/>
+                            <col width="0%"/>
+                            <col width="0%"/>
+                            <col width="0%"/>
+                        </colgroup>
+                        <x-pulse::thead>
+                            <tr>
+                                <x-pulse::th>Event</x-pulse::th>
+                                <x-pulse::th class="text-right whitespace-nowrap">Quantity</x-pulse::th>
+                            </tr>
+                        </x-pulse::thead>
+                        <tbody>
+                        @foreach ($chartData as $key => $interaction)
+                            <tr wire:key="spacer-{{ $interaction['label'] }}" class="h-2 first:h-0"></tr>
+                            <tr wire:key="row-{{ $interaction['label'] }}">
+                                <x-pulse::td class="max-w-[1px]">
+                                    <code class="block text-xs text-gray-900 dark:text-gray-100 truncate"
+                                          title="{{ $interaction['label'] }}">
+                                        {{ $interaction['label'] }}
+                                    </code>
+                                </x-pulse::td>
+                                <x-pulse::td numeric class="text-gray-700 dark:text-gray-300 font-bold">
+                                    {{ (int) $interaction['data'][0] }} {{-- Assuming 'data' is always an array with one element --}}
+                                </x-pulse::td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </x-pulse::table>
+                </div>
+            </div>
+        </x-pulse::scroll>
+    @endif
+
 </x-pulse::card>
 
 <script>
@@ -18,15 +62,15 @@
             },
             options: {
                 scales: {
-                    x: { stacked: true },
-                    y: { stacked: true, beginAtZero: true }
+                    x: {stacked: true},
+                    y: {stacked: true, beginAtZero: true}
                 },
                 plugins: {
-                    legend: { display: false }
+                    legend: {display: false}
                 },
                 responsive: true,
                 maintainAspectRatio: false,
-                interaction: { intersect: false }
+                interaction: {intersect: false}
             }
         });
     });
